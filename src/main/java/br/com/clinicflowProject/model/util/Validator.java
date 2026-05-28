b2ef8a872ea9d04fc1aa2e8f4b9cc8df292aa8f3
Kauã Menezes <kauavictor.f.menezes@hotmail.com>
2026-05-28 02:22:55 -0300

    feat: implementação da classe Validator, responsavel pelas validações do sistema

---
+++ src/main/java/br/com/clinicflowProject/model/util/Validator.java
@@ -0,0 +1,62 @@
+package br.com.clinicflowProject.model.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public final class Validator {
+
+    private Validator() {
+    }
+
+    public static void requireNonNull(Object object, String fieldName) {
+        if (object == null) {
+            throw new IllegalArgumentException(fieldName + " não pode ser nulo.");
+        }
+    }
+
+    public static void requireNonBlank(String value, String fieldName) {
+        requireNonNull(value, fieldName);
+        if (value.trim().isEmpty()) {
+            throw new IllegalArgumentException(fieldName + "não pode estar vazio.");
+        }
+    }
+
+    public static void requireValidTextFormat(String value, int min, int max, String fieldName) {
+        requireNonNull(value, fieldName);
+        requireNonBlank(value, fieldName);
+        if (value.length() < min || value.length() > max) {
+            throw new IllegalArgumentException(fieldName + " deve conter entre: " + min + " e " + max + " caracteres.");
+        }
+    }
+
+    public static void requireValidDocumentFormat(String value, String fieldName) {
+        requireNonNull(value, fieldName);
+        requireNonBlank(value, fieldName);
+        if (!value.matches("\\d{11}")) {
+            throw new IllegalArgumentException(fieldName + " deve conter exatamente 11 digitos.");
+        }
+    }
+
+
+    public static void requireValidCrmFormat(String value, String fieldName) {
+        requireNonNull(value, fieldName);
+        requireNonBlank(value, fieldName);
+        if (!value.matches("\\d{6}")) {
+            throw new IllegalArgumentException(fieldName + " deve conter exatamente 6 digitos.");
+        }
+    }
+
+    public static void requirePastOrPresentDate(LocalDate date, String fieldName) {
+        requireNonNull(date, fieldName);
+        if (date.isAfter(LocalDate.now())) {
+            throw new IllegalArgumentException(fieldName + " não pode estar no futuro.");
+        }
+    }
+
+    public static void requirePastOrPresentDateTime(LocalDateTime dateTime, String fieldName) {
+        requireNonNull(dateTime, fieldName);
+        if (dateTime.isAfter(LocalDateTime.now())) {
+            throw new IllegalArgumentException(fieldName + " não pode estar no futuro.");
+        }
+    }
+}
